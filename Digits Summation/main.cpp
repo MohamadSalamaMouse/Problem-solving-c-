98591c02eae57e1035525f16442584662918364f
MohamedSalamaMouse <slamtm608@gmail.com>
2023-01-26 20:07:36 +0200

    Digits Summation

---
+++ Digits Summation/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long num1,num2;
+    cin>>num1>>num2;
+   int n1 =num1%10;
+int n2=num2%10;
+
+   cout<<n1+n2;
+    return 0;
+}
