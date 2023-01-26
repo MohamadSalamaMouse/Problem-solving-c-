843c4c10af23d5437d2ead247b3d31cded19107b
MohamedSalamaMouse <slamtm608@gmail.com>
2023-01-26 18:24:16 +0200

    simpleCalculator&hello&data

---
+++ Basic Data Types/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    long long y;
+    char z;
+    float n;
+    double d;
+    cin>>x>>y>>z>>n>>d;
+    cout<<x<<"\n"<<y<<"\n"<<z<<"\n"<<n<<"\n"<<d;
+    return 0;
+}
